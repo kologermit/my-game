6f49ba033b85b9684919219b1c9002e1d9e9933c
kologermanskyi <kologermit@gmail.com>
2021-11-28 15:05:19 +0400

    first_commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+  <head>
+    <link href="./node_modules/nes.css/css/nes-core.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet">
+  </head>
+  <body>
+  </body>
+</html>
